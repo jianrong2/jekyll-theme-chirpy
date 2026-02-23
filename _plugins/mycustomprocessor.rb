3153cd8b03e8acd875903943d30e6d8c37b961e8
Jianrong Qiu <jianrongqiu9@gmail.com>
2026-02-23 17:13:09 +0000

    feat: use new image folder

--- _plugins/mycustomprocessor.rb
+++ _plugins/mycustomprocessor.rb
@@ -1,9 +1,7 @@
 class Jekyll::Converters::Markdown::MyCustomProcessor < Jekyll::Converters::Markdown::KramdownParser
   def convert(content)
-    new_content = content.gsub('](../img/', '](/img/')
-    new_content = new_content.gsub('](../../img/', '](/img/')
-	new_content = new_content.gsub('<img src="../img/', '<img src="/img/')
-    new_content = new_content.gsub('<img src="../../img/', '<img src="/img/')
+    new_content = content.gsub(/^!\[(.+)\]\(\.\.\/\.\.\/img/,'![\1](/img/')
+    new_content = new_content.gsub(/^<img src="\.\.\/\.\.\/img/, '<img src="/img/')
     super(new_content)
   end
 end
