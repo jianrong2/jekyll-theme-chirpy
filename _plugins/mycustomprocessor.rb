d8500e2d0a8f4c118f3df0eea98d47b1713e61c7
Jianrong Qiu <jianrongqiu9@gmail.com>
2026-02-27 14:48:43 +0000

    fix: display issues of equations and flowchart

--- _plugins/mycustomprocessor.rb
+++ _plugins/mycustomprocessor.rb
@@ -2,6 +2,7 @@ class Jekyll::Converters::Markdown::MyCustomProcessor < Jekyll::Converters::Mark
   def convert(content)
     new_content = content.gsub(/^!\[(.+)\]\(\.\.\/\.\.\/img/,'![\1](/img/')
     new_content = new_content.gsub(/^<img src="\.\.\/\.\.\/img/, '<img src="/img/')
+    new_content = new_content.gsub(/^\$\$$/,"\n\$\$\n")
     super(new_content)
   end
 end
