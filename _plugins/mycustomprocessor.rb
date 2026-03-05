4c6f3876b33c19fd1ec65668f0b0806d1be829cc
Jianrong Qiu <jianrongqiu9@gmail.com>
2026-03-05 12:10:09 +0000

    fix: ignore space and tab in front of the math delineator

--- _plugins/mycustomprocessor.rb
+++ _plugins/mycustomprocessor.rb
@@ -1,8 +1,6 @@
 class Jekyll::Converters::Markdown::MyCustomProcessor < Jekyll::Converters::Markdown::KramdownParser
   def convert(content)
-    new_content = content.gsub(/^!\[(.+)\]\(\.\.\/\.\.\/img/,'![\1](/img/')
-    new_content = new_content.gsub(/^<img src="\.\.\/\.\.\/img/, '<img src="/img/')
-    new_content = new_content.gsub(/^\$\$$/,"\n\$\$\n")
+    new_content = content.gsub(/^!\[(.+)\]\(\.\.\/\.\.\/img/,'![\1](/img/').gsub(/^<img src="\.\.\/\.\.\/img/, '<img src="/img/').gsub(/^[[:blank:]]*?\$\$$/,"\n$$\n")
     super(new_content)
   end
 end
