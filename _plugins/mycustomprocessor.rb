bb28a8999f31b44a3b966885868dc3be6dc36c57
Jianrong Qiu <jianrongqiu9@gmail.com>
2026-04-27 15:52:47 +0100

    feat: rewrite base path ../img and ./img to /img as well

--- _plugins/mycustomprocessor.rb
+++ _plugins/mycustomprocessor.rb
@@ -1,6 +1,6 @@
 class Jekyll::Converters::Markdown::MyCustomProcessor < Jekyll::Converters::Markdown::KramdownParser
   def convert(content)
-    new_content = content.gsub(/^!\[(.+)\]\(\.\.\/\.\.\/img/,'![\1](/img/').gsub(/^<img src="\.\.\/\.\.\/img/, '<img src="/img/').gsub(/^[[:blank:]]*?\$\$$/,"\n$$\n")
+    new_content = content.gsub(/^!\[(.+)\]\((?:\.\.\/\.\.\/|\.\.\/|\.\/)img/, '![\1](/img').gsub(/^<(img|video) src="(?:\.\.\/\.\.\/|\.\.\/|\.\/)img/, '<\1 src="/img').gsub(/^[[:blank:]]*?\$\$$/,"\n$$\n")
     super(new_content)
   end
 end
